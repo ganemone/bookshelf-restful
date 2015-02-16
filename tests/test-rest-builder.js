06995c3a8d48d8bbc19a6cee3e729d67dcfd9cbf
Giancarlo Anemone <ganemone@gmail.com>
2015-02-16 10:27:33 -0500

    Implement test for get endpoints

--- tests/test-rest-builder.js
+++ tests/test-rest-builder.js
@@ -3,13 +3,41 @@ var rewire = require('rewire');
 var RestBuilder = rewire('../lib/rest-builder');
 var mock = require('./util/mock');
 var mockDB = mock(require('../lib/db'));
-var server, bookshelf, client = require('./util/shared');
+var shared = require('./util/shared');
+var user = require('./util/models/user');
+var noarg = require('./util/noarg');
+
+mockDB.get.addSideEffect(function() {
+  var args = Array.prototype.slice.call(arguments);
+  var cb = args.pop();
+  cb(null, {});
+});
 
 RestBuilder.__set__('db', mockDB);
 
-describe('RestBuilder', function() {
-  var rb = new RestBuilder(server);
-  describe('defineGetSingle', function() {
+function setUpRestBuilder() {
+  beforeEach(function(done) {
+    this.server = shared.createServer();
+    this.rb = new RestBuilder(this.server);
+    this.server.listen(8080, noarg(done));
+  });
+  afterEach(function() {
+    this.server.close();
+  });
+}
 
+describe('RestBuilder', function() {
+  describe('defineGetSingle', function() {
+    describe('no processors', function() {
+      setUpRestBuilder();
+      it('should call db.get when a request comes it', function(done) {
+        this.rb.defineGetSingle('users', user, [], []);
+        shared.client.get('/users/1', function(err, req, res, obj) {
+          assert.ifError(err);
+          mockDB.get.assertCalledOnceWithArgsIncluding(['1', user]);
+          done();
+        });
+      });
+    });
   });
 });
